c01c485d6d8aa667ee45e86b2708a1c1363ef270
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2020-03-01 22:24:18 -0500

    FormGroup, formControlName, Validators

--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  fgValidation: FormGroup; /* grupo de validación de formulario */
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) { 
+
+  }
+
+  fgValidationBuilder(){
+    this.fgValidation = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(30), Validators.email]],
+      password: ['', Validators.required, Validators.minLength(8), Validators.maxLength(15)]
+    });
+  }
+
+  ngOnInit() {
+    this.fgValidationBuilder(); 
+  }
+  
+  loginEvent(){
+    if (this.fgValidation.invalid){
+      alert("Datos Inválidos");
+    }else{
+      alert("yendo al login");
+    }
+  }
+
+  get fgv(){
+    return this.fgValidation.controls;
   }
 
 }
